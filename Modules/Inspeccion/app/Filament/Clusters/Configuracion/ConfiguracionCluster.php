21dffad2148c26cc557f1fa02161cc7ea56b66de
discorallado <sebastian.alvarez.c@usach.cl>
2026-08-03 06:52:40 -0400

    fix: completa ADR 0018 (SubNavigationPosition faltante en ConfiguracionCluster)
    Cambio quedó sin commitear de una sesión anterior: agrega
    SubNavigationPosition::End al cluster de Configuración, tal como
    documenta el ADR 0018 (reordenamiento de sidebar).

--- Modules/Inspeccion/app/Filament/Clusters/Configuracion/ConfiguracionCluster.php
+++ Modules/Inspeccion/app/Filament/Clusters/Configuracion/ConfiguracionCluster.php
@@ -4,6 +4,7 @@ namespace Modules\Inspeccion\Filament\Clusters\Configuracion;
 
 use BackedEnum;
 use Filament\Clusters\Cluster;
+use Filament\Pages\Enums\SubNavigationPosition;
 use Filament\Support\Icons\Heroicon;
 
 class ConfiguracionCluster extends Cluster
@@ -11,6 +12,8 @@ class ConfiguracionCluster extends Cluster
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCog6Tooth;
 
     protected static ?int $navigationSort = 4;
+
+    protected static ?SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
 
     public static function getNavigationLabel(): string
     {
